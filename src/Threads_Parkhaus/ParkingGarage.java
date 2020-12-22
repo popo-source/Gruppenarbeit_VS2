b3bd22c67ec159f57e5b082621a529801520ccea
paulh <paul.hauser013@gmail.com>
2020-12-22 11:20:28 +0100

    Treads_Parkhaus update 2

--- src/Threads_Parkhaus/ParkingGarage.java
+++ src/Threads_Parkhaus/ParkingGarage.java
@@ -1,7 +1,16 @@
 package Threads_Parkhaus;
+import TastaturRea;
 
 public class ParkingGarage {
+    int platz = 0;
 
+    public static void main(String[] args) {
+        System.out.println("Veuillez entrer un nombre de place:");
+        do{
+            platz=TastaturRead.
+        }while(platz>0 && platz<=100)
+
+    }
 
 
 
