95285c5f6d155288fb6fa363c108250543da3a95
paulh <paul.hauser013@gmail.com>
2020-12-28 12:33:50 +0100

    Treads_Parkhaus update 7

--- src/Threads_Parkhaus/Parking.java
+++ src/Threads_Parkhaus/Parking.java
@@ -3,29 +3,46 @@ package Threads_Parkhaus;
 import java.util.Scanner;
 
 public class Parking {
-    //Scanner scanner = new Scanner(System.in);
-    static Car[] cars = new Car[10];
+    Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Welcome to the new Parking,\n Choose a parking size (number of places):");
         ParkingGarage garage = new ParkingGarage();
-        garage.parkplace=10;
+        System.out.println("Wirkommen in unseren Parkhaus");
 
-        for(int i=0;i<10;i++){
+        //Optionale eingabe:
+
+        /*
+        System.out.print("Wählen sie eine anzahl von parkplätze:");
+        garage.parkplace=input();
+        System.out.print("Wählen sie eine anzahl von Autos:");
+        int autos = input();
+        */
+
+        garage.parkplace = 10;
+        int autos = 10;
+        Car[] cars = new Car[autos];
+
+        for(int i=0;i<autos;i++){
             cars[i] = new Car(i+1);
             cars[i].setGarage(garage);
             cars[i].start();
         }
 
-        /*
-        do{
-            garage.parkplace = TastaturRead.readLong();
-            if(garage.parkplace<=0){
-                System.out.println("Fehler bei der eingabe!");
-            }
-        } while(garage.parkplace<=0);
-        */
+    }
 
+    public static int input(){
+        int input = 0;
+        boolean valid = false;
+        do{
+            input = (int) TastaturRead.readLong();
+            if(input<=0){
+                System.out.println("!!! Eingabe nicht möglich !!!");
+                System.out.print("Geben sie nochmal ein zahl ein:");
+            } else {
+                valid = true;
+            }
+        }while(valid==false);
+        return input;
     }
 
 }
